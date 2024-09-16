2634a74c7b6d19d6f0daf040362e417becbc1361
Anna <azeleranskaya@gmail.com>
2024-09-16 16:57:50 +0300

    apply linters and finally edition HW2

--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,21 @@
-from masks import get_mask_card_number
-from masks import get_mask_account
+from datetime import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(input_number: str) -> str:
+    """Функция принимает тип и номер карты или счета и возвращает тип карты или счета и замаскированный номер"""
+    result = ""
     if input_number.startswith("Счет"):
-        result = "Счет " + get_mask_account(input_number)
+        result += "Счет " + get_mask_account(input_number)
     else:
-        result = input_number[:-16] + get_mask_card_number(input_number[-16:])
+        result += input_number[:-16] + get_mask_card_number(input_number[-16:])
     return result
 
 
-def get_date(input_date: str) -> str:
-    date = input_date[8:10] + "." + input_date[5:7] + "." + input_date[:4]
-    return date
+def get_date(user_date: str) -> str:
+    """Функция принимает дату в заданном формате и возвращает дату в формате ДД.ММ.ГГГГ."""
+    date_format = datetime.strptime(user_date, "%Y-%m-%dT%H:%M:%S.%f")
+    new_date = date_format.strftime("%d.%m.%Y")
+    # new_date = input_date[8:10] + "." + input_date[5:7] + "." + input_date[:4]
+    return new_date
