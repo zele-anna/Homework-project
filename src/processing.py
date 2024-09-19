aa7d40cc1c99ae68483a85032eaec94f2924247f
Anna <azeleranskaya@gmail.com>
2024-09-19 14:17:56 +0300

    add type annotation for param state in func filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def filter_by_state(id_info: List[dict], state="EXECUTED") -> List[dict]:
+def filter_by_state(id_info: List[dict], state: str = "EXECUTED") -> List[dict]:
     """Функция фильтрует информацию о id по ключу state (по умолчанию - EXECUTED)."""
     return [item for item in id_info if item["state"] == state]
 
