acb347816116b5a95179c94d92558a05190ddd92
Anna <azeleranskaya@gmail.com>
2024-09-19 14:14:29 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -4,3 +4,8 @@ from typing import List
 def filter_by_state(id_info: List[dict], state="EXECUTED") -> List[dict]:
     """Функция фильтрует информацию о id по ключу state (по умолчанию - EXECUTED)."""
     return [item for item in id_info if item["state"] == state]
+
+
+def sort_by_date(id_info: List[dict], is_reverse: bool = True) -> List[dict]:
+    """Функция сортирует информацию о id по дате (по умолчанию - по убыванию)."""
+    return sorted(id_info, key=lambda x: x["date"], reverse=is_reverse)
