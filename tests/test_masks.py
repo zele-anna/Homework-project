06c6fe826714870f853ad93cdf917f2f5e39e18c
Anna <azeleranskaya@gmail.com>
2024-09-23 13:02:14 +0300

    homework 10-2 done

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -20,15 +20,16 @@ def test_get_mask_card_number_value_error():
     assert str(exc_info.value) == "Введен некорректный номер"
 
 
-def test_get_mask_account():
-    assert get_mask_account("73654108430135874305") == "**4305"
-    assert get_mask_account(73654108430135874305) == "**4305"
+@pytest.mark.parametrize("account, expected", [("73654108430135874305", "**4305"), (73654108430135874305, "**4305")])
+def test_get_mask_account(account, expected):
+    assert get_mask_account(account) == expected
 
 
 def test_get_mask_account_incorrect_number():
     with pytest.raises(ValueError) as exc_info:
         get_mask_account("")
         get_mask_account(0)
+        get_mask_account(None)
         get_mask_account("123")
         get_mask_account("12345678")
         get_mask_account("1000200030004000123")
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,8 +1,6 @@
 import pytest
-from mypy.types import NoneTyp
 
-from src.processing import filter_by_state
-from src.processing import sort_by_date
+from src.processing import filter_by_state, sort_by_date
 
 
 @pytest.fixture
@@ -11,7 +9,7 @@ def id_list():
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
 
@@ -44,46 +42,67 @@ def test_filter_by_state_state_not_found():
         filter_by_state([{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}], "CANCELED")
 
 
-@pytest.mark.parametrize("is_reverse, expected", [
-    (True,
-     [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
-    (False,
-     [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-      {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]),
-    (None,
-     [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-      {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}])])
+@pytest.mark.parametrize(
+    "is_reverse, expected",
+    [
+        (
+            True,
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            False,
+            [
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            ],
+        ),
+        (
+            None,
+            [
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            ],
+        ),
+    ],
+)
 def test_sort_by_date(id_list, is_reverse, expected):
     assert sort_by_date(id_list, is_reverse) == expected
 
 
 def test_sort_by_date_equal_dates():
-    assert sort_by_date([
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
-    ],
-        True) == [
+    assert sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ],
+        True,
+    ) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     ]
 
 
 def test_sort_by_date_date_format():
-    assert sort_by_date([
+    assert sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "07.03.2019"},
+            {"id": 594226727, "state": "CANCELED", "date": "12/09/2018"},
+        ],
+        True,
+    ) == [
+        {"id": 594226727, "state": "CANCELED", "date": "12/09/2018"},
         {"id": 41428829, "state": "EXECUTED", "date": "07.03.2019"},
-        {"id": 594226727, "state": "CANCELED", "date": "12.09.2018"}
-    ],
-        True) == [
-        {"id": 594226727, "state": "CANCELED", "date": "12.09.2018"},
-        {"id": 41428829, "state": "EXECUTED", "date": "07.03.2019"},]
+    ]
