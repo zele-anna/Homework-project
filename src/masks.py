523ba07266c30b711383f2ac0aa7d54bffd6fb42
Anna <azeleranskaya@gmail.com>
2024-09-23 12:26:51 +0300

    add test for func sort_by_date

--- src/masks.py
+++ src/masks.py
@@ -2,7 +2,7 @@ def get_mask_card_number(card_number: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску,
     в которой скрыты символы с 7 по 12"""
 
-    if isinstance(card_number):
+    if isinstance(card_number, int):
         card_number = str(card_number)
 
     if len(card_number) == 0:
@@ -19,7 +19,7 @@ def get_mask_account(account_number: str) -> str:
     """Принимает на вход номер счета и возвращает его маску,
     в которой отображаются только последние 4 цифры номера счета"""
 
-    if isinstance(account_number):
+    if isinstance(account_number, int):
         account_number = str(account_number)
 
     if len(account_number) != 20:
