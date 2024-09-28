311cd73ab534edac733617632a354088d7baad5f
Anna <azeleranskaya@gmail.com>
2024-09-29 01:11:51 +0300

    add func transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,2 +1,40 @@
-def filter_by_currency(transaction_dict, currency):
-    return (x for x in transaction_dict if x["operationAmount"]["currency"]["name"] == currency)
+def filter_by_currency(transaction_list, currency):
+    return (x for x in transaction_list if x["operationAmount"]["currency"]["name"] == currency)
+
+
+def transaction_descriptions(transaction_list):
+    return (x["description"] for x in transaction_list)
+
+
+# transactions = [
+#     {
+#         "id": 939719570,
+#         "state": "EXECUTED",
+#         "date": "2018-06-30T02:08:58.425572",
+#         "operationAmount": {
+#             "amount": "9824.07",
+#             "currency": {
+#                 "name": "USD",
+#                 "code": "USD"
+#             }
+#         },
+#         "description": "Перевод организации",
+#         "from": "Счет 75106830613657916952",
+#         "to": "Счет 11776614605963066702"
+#     },
+#     {
+#         "id": 142264268,
+#         "state": "EXECUTED",
+#         "date": "2019-04-04T23:20:05.206878",
+#         "operationAmount": {
+#             "amount": "79114.93",
+#             "currency": {
+#                 "name": "USD",
+#                 "code": "USD"
+#             }
+#         },
+#         "description": "Перевод со счета на счет",
+#         "from": "Счет 19708645243227258542",
+#         "to": "Счет 75651667383060284188"
+#     }
+# ]
