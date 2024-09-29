1c63c23c42d3f7433640a21f759908893dbd2acd
Anna <azeleranskaya@gmail.com>
2024-09-29 21:52:50 +0300

    add func card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -1,9 +1,21 @@
+from turtledemo.penrose import start
+
+
 def filter_by_currency(transaction_list, currency):
     return (x for x in transaction_list if x["operationAmount"]["currency"]["name"] == currency)
 
 
 def transaction_descriptions(transaction_list):
     return (x["description"] for x in transaction_list)
+
+
+def card_number_generator(start_num, stop_num):
+    for number in range(start_num, stop_num+1):
+        new_card_number = str(number)
+        while len(new_card_number) < 16:
+            new_card_number = "0" + new_card_number
+        new_card_number = f"{new_card_number[0:4]} {new_card_number[4:8]} {new_card_number[8:12]} {new_card_number[12:16]}"
+        yield new_card_number
 
 
 # transactions = [
