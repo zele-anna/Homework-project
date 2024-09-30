e2887398f527af174f7a4b32a55e8da9501b236c
Anna <azeleranskaya@gmail.com>
2024-09-30 10:26:41 +0300

    add parametried tests and coverage report

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -45,36 +45,51 @@ def transactions():
     ]
 
 
-def test_filter_by_currency(transactions):
-    a = filter_by_currency(transactions, "USD")
-    b = filter_by_currency(transactions, "EUR")
-    assert next(a) == {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    }
-    assert next(a) == {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
-    }
-    assert next(b) == {
-        "id": 939719572,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "EUR", "code": "EUR"}},
-        "description": "Перевод с карты на карту",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    }
+@pytest.mark.parametrize(
+    "currency, expected",
+    [
+        (
+            "USD",
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод организации",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                },
+                {
+                    "id": 142264268,
+                    "state": "EXECUTED",
+                    "date": "2019-04-04T23:20:05.206878",
+                    "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод со счета на счет",
+                    "from": "Счет 19708645243227258542",
+                    "to": "Счет 75651667383060284188",
+                },
+            ],
+        ),
+        (
+            "EUR",
+            [
+                {
+                    "id": 939719572,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "operationAmount": {"amount": "9824.07", "currency": {"name": "EUR", "code": "EUR"}},
+                    "description": "Перевод с карты на карту",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                }
+            ],
+        ),
+    ],
+)
+def test_filter_by_currency(transactions, currency, expected):
+    a = filter_by_currency(transactions, currency)
+    assert list(a) == expected
 
 
 def test_filter_by_currency_not_found(transactions):
@@ -103,22 +118,18 @@ def test_transaction_descriptions_empty():
     assert str(exc_info.value) == "Список транзакций не задан"
 
 
-# @pytest.mark.parametrize("start, end, expected", [1, 1, "0000 0000 0000 00001"])
-def test_card_number_generator():
-    a = card_number_generator(1, 3)
-    assert next(a) == "0000 0000 0000 0001"
-    assert next(a) == "0000 0000 0000 0002"
-    assert next(a) == "0000 0000 0000 0003"
-    b = card_number_generator(9999, 10001)
-    assert next(b) == "0000 0000 0000 9999"
-    assert next(b) == "0000 0000 0001 0000"
-    assert next(b) == "0000 0000 0001 0001"
-    c = card_number_generator(999999999999, 1000000000001)
-    assert next(c) == "0000 9999 9999 9999"
-    assert next(c) == "0001 0000 0000 0000"
-    assert next(c) == "0001 0000 0000 0001"
-    d = card_number_generator(9999999999999999, 9999999999999999)
-    assert next(d) == "9999 9999 9999 9999"
+@pytest.mark.parametrize(
+    "start, end, expected",
+    [
+        (1, 3, ["0000 0000 0000 0001", "0000 0000 0000 0002", "0000 0000 0000 0003"]),
+        (9999, 10001, ["0000 0000 0000 9999", "0000 0000 0001 0000", "0000 0000 0001 0001"]),
+        (999999999999, 1000000000001, ["0000 9999 9999 9999", "0001 0000 0000 0000", "0001 0000 0000 0001"]),
+        (9999999999999998, 9999999999999999, ["9999 9999 9999 9998", "9999 9999 9999 9999"]),
+    ],
+)
+def test_card_number_generator(start, end, expected):
+    a = card_number_generator(start, end)
+    assert list(a) == expected
 
 
 def test_card_number_generator_out_of_range():
