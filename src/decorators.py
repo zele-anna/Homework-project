6f977dc6431134bed8284fbf146ada57d5cd722e
Anna <azeleranskaya@gmail.com>
2024-10-01 23:50:02 +0300

    add test for decorator log and small fixes

--- src/decorators.py
+++ src/decorators.py
@@ -1,25 +1,29 @@
 from functools import wraps
+from typing import Callable, Optional
 
 
-def log(filename="no_file"):
+def log(filename: Optional[str] = "no_file") -> Callable:
     """Декоратор, который проверяет работу функции и записывает лог с результатами в файл,
     имя которого опционально задается в параметрах декоратора, либо выводит лог в консоль,
     если наименование файла не задано."""
-    def decorator(func):
+
+    def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
-                result = func(*args, **kwargs)
+                func(*args, **kwargs)
                 if filename == "no_file":
                     print(f"{func.__name__} ok")
                 else:
-                    with open(filename, "w", encoding="UTF-8") as file:
-                        file.write(f"{func.__name__} ok")
+                    with open(filename, "a", encoding="UTF-8") as file:
+                        file.write(f"{func.__name__} ok\n")
             except Exception as er_info:
                 if filename == "no_file":
                     print(f"{func.__name__} error: {er_info.__class__.__name__}. Inputs: {args}, {kwargs}")
                 else:
-                    with open(filename, "w", encoding="UTF-8") as file:
-                        file.write(f"{func.__name__} error: {er_info.__class__.__name__}. Inputs: {args}, {kwargs}")
+                    with open(filename, "a", encoding="UTF-8") as file:
+                        file.write(f"{func.__name__} error: {er_info.__class__.__name__}. Inputs: {args}, {kwargs}\n")
+
         return wrapper
+
     return decorator
---
+++ tests/test_decorators.py
@@ -0,0 +1,33 @@
+import pytest
+
+from src.decorators import log
+
+
+def test_log_console(capsys):
+    @log()
+    def my_function(x: int, y: int) -> int:
+        return x + y
+
+    my_function(2, 3)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function ok\n"
+
+
+def test_log_console_division_error(capsys):
+    @log()
+    def my_function(x: int, y: int) -> float:
+        return x / y
+
+    my_function(2, 0)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function error: ZeroDivisionError. Inputs: (2, 0), {}\n"
+
+
+def test_log_console_type_error_(capsys):
+    @log()
+    def my_function(x: int, y: list) -> list:
+        return [x for x in y]
+
+    my_function(2, 5)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function error: TypeError. Inputs: (2, 5), {}\n"
