5f83ca89f3f01a75ec1c81d615fd4a6db3a36981
Anna <azeleranskaya@gmail.com>
2024-10-25 23:25:39 +0300

    add seach.py funcs

---
+++ src/seach.py
@@ -0,0 +1,22 @@
+import re
+from collections import Counter
+
+
+def search_via_description(transactions: list, string_to_find: str) -> list:
+    """Функция принимает список словарей с данными о банковских операциях и строку поиска, а возвращает список словарей,
+    у которых в описании есть данная строка"""
+    found_transactions = []
+    for transaction in transactions:
+        if re.findall(string_to_find.lower(), str(transaction.get('description')).lower()):
+            found_transactions.append(transaction)
+    return found_transactions
+
+
+def category_counter(transactions: list, categories: list) -> dict:
+    categories_lower = [cat.lower() for cat in categories]
+    transactions_to_count = []
+    for transaction in transactions:
+        if str(transaction.get('description')).lower() in categories_lower:
+            transactions_to_count.append(transaction.get('description'))
+    counted_categories = Counter(transactions_to_count)
+    return counted_categories
